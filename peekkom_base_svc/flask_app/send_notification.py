98b64a8856622e1b98ddc29d4e8598ff40786e55
eyjang <sa358@naver.com>
2025-07-08 00:29:02 +0900

    modified linked error

--- peekkom_base_svc/flask_app/send_notification.py
+++ peekkom_base_svc/flask_app/send_notification.py
@@ -2,32 +2,17 @@ import firebase_admin
 from firebase_admin import credentials, messaging
 
 def send_fcm_notification(title, body):
-#     load_dotenv()
-#     fcm_key = os.getenv("FCM_SERVER_KEY")
-#     device_token = os.getenv("DEVICE_TOKEN")
-#     headers = {
-#         'Authorization': f'key={fcm_key}',
-#         'Content-Type': 'application/json',
-#     }
-#     payload = {
-#         'to': device_token,
-#         'notification': {
-#             'title': title,
-#             'body': body,
-#         }
-#     }
-#     response = requests.post(FCM_API_URL, headers=headers, json=payload)
     if not firebase_admin._apps:
         cred = credentials.Certificate("firebase-adminsdk.json")
         firebase_admin.initialize_app(cred)
 
-    condition = "'stock-GOOG' in topics || 'industry-tech' in topics"
+    // TODO - Device token 처리 변경
     message = messaging.Message(
+        token='djeqyyhDSjet53TcMH_8g0:APA91bEqU6f5kwwwq9tXx6CZO-k5kPZ2guDccLblrUlD4XuXdzR9axmOw37c6QOoWJTvfA3jkT67P5PeM1n0p6Wn03U2WKVkWB7DxvnDTWcWVXWePpL3lfE',
         notification=messaging.Notification(
-            title = title,
-            body = body,
+            title=title,
+            body=body
         ),
-        condition=condition,
     )
     response = messaging.send(message)
     print('Successfully sent message:', response)
